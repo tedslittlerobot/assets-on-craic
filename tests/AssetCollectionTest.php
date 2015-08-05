5f8973e2d1ce8cd041f44d3ee644f785854011fb
Stef Horner <stef@tedslittlerobot.com>
2015-08-05 13:33:41 +0100

    more tests for assets

---
+++ tests/AssetCollectionTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use Mockery as m;
+use Tlr\Assets\AssetCollection;
+use Tlr\Assets\AssetResolver;
+use Tlr\Assets\Definitions\Asset;
+
+class AssetCollectionTest extends PHPUnit_Framework_TestCase {
+
+    public function tearDown() {
+        m::close();
+    }
+
+    public function setUp() {
+        $this->collection = new AssetCollection;
+    }
+
+    public function testRegisterCallable() {
+        $this->collection->register('foo', $cb = function() {});
+
+        $this->assertSame(['foo' => $cb], $this->collection->getCallables());
+    }
+
+    public function testBasicResolve() {
+        $timesCalled = 0;
+        $this->collection->register('foo', $cb = function(Asset $asset) use (&$timesCalled) {
+            $timesCalled++;
+        });
+
+        $asset = $this->collection->get('foo');
+
+        $this->assertTrue($asset instanceof Asset);
+        $this->assertEquals(1, $timesCalled);
+    }
+
+    public function testCachedResolve() {
+        $timesCalled = 0;
+        $this->collection->register('foo', $cb = function(Asset $asset) use (&$timesCalled) {
+            $timesCalled++;
+        });
+
+        $first = $this->collection->get('foo');
+        $second = $this->collection->get('foo');
+
+        $this->assertTrue($first instanceof Asset);
+        $this->assertTrue($second instanceof Asset);
+        $this->assertSame($first, $second);
+        $this->assertEquals(1, $timesCalled);
+    }
+
+}
--- tests/AssetResolverTest.php
+++ tests/AssetResolverTest.php
@@ -7,22 +7,67 @@ use Tlr\Assets\Definitions\Asset;
 
 class AssetResolverTest extends PHPUnit_Framework_TestCase {
 
-	public function tearDown() {
-		m::close();
-	}
+    public function tearDown() {
+        m::close();
+    }
 
-	public function setUp() {
-		$this->assets = m::mock(AssetCollection::class);
-		$this->resolver = new AssetResolver($this->assets);
-	}
+    public function setUp() {
+        $this->assets = m::mock(AssetCollection::class);
+        $this->resolver = new AssetResolver($this->assets);
+    }
 
-	public function testBasicResolve() {
-		$this->assets->shouldReceive('get')->once()->with('foo')->andReturn($asset = m::mock(Asset::class));
-		$asset->shouldReceive('dependancies')->once()->with()->andReturn([]);
+    public function testBasicResolve() {
+        $this->assets->shouldReceive('get')->once()->with('foo')->andReturn($asset = m::mock(Asset::class));
+        $asset->shouldReceive('dependancies')->once()->with()->andReturn([]);
 
-		$this->resolver->resolve('foo');
+        $this->resolver->resolve('foo');
 
-		$this->assertSame([$asset], $this->resolver->assets());
-	}
+        $this->assertSame([$asset], $this->resolver->assets());
+    }
 
+    public function testBasicDependanciesResolve() {
+        $this->assets->shouldReceive('get')->once()->with('foo')->andReturn($asset = m::mock(Asset::class));
+        $this->assets->shouldReceive('get')->once()->with('bar')->andReturn($dependancy = m::mock(Asset::class));
+
+        $asset->shouldReceive('dependancies')->once()->with()->andReturn(['bar']);
+        $dependancy->shouldReceive('dependancies')->once()->with()->andReturn([]);
+
+        $this->resolver->resolve('foo');
+
+        $this->assertSame([$dependancy, $asset], $this->resolver->assets());
+    }
+
+    public function testBasicDependanciesOnlyResolveOnce() {
+        $this->assets->shouldReceive('get')->times(2)->with('foo')->andReturn($asset = m::mock(Asset::class));
+        $asset->shouldReceive('dependancies')->once()->with()->andReturn([]);
+
+        $this->resolver->resolve('foo');
+        $this->resolver->resolve('foo');
+
+        $this->assertSame([$asset], $this->resolver->assets());
+    }
+
+    public function testArrayResolves() {
+        $this->assets->shouldReceive('get')->once()->with('foo')->andReturn($first = m::mock(Asset::class));
+        $this->assets->shouldReceive('get')->once()->with('bar')->andReturn($second = m::mock(Asset::class));
+        $first->shouldReceive('dependancies')->once()->with()->andReturn([]);
+        $second->shouldReceive('dependancies')->once()->with()->andReturn([]);
+
+        $this->resolver->resolveArray(['foo', 'bar']);
+
+        $this->assertSame([$first, $second], $this->resolver->assets());
+    }
+
+    public function testClearResolved() {
+        $this->assets->shouldReceive('get')->once()->with('foo')->andReturn($first = m::mock(Asset::class));
+        $first->shouldReceive('dependancies')->once()->with()->andReturn([]);
+
+        $this->resolver->resolve('foo');
+
+        $this->assertSame([$first], $this->resolver->assets());
+
+        $this->resolver->clear();
+
+        $this->assertSame([], $this->resolver->assets());
+    }
 }
