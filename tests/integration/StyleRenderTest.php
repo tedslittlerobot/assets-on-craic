7b37ea7d517d0e9fd1ee956c9de0a2c0a7e39ca6
Stef Horner <stef@tedslittlerobot.com>
2015-08-17 15:49:41 +0100

    test more renders

---
+++ tests/integration/StyleRenderTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use Mockery as m;
+use Tlr\Assets\AssetCollection;
+use Tlr\Assets\AssetRenderer;
+use Tlr\Assets\AssetResolver;
+
+class StyleRenderTest extends PHPUnit_Framework_TestCase {
+
+    public function tearDown() {
+        m::close();
+    }
+
+    public function setUp() {
+        $this->assets = new AssetCollection;
+        $this->resolver = new AssetResolver($this->assets);
+        $this->renderer = new AssetRenderer;
+    }
+
+    public function testBasicRender() {
+        $this->assets->register('foo', function($asset) {
+            $asset->style()
+                ->file(__DIR__ . '/fixtures/foo.css');
+        });
+        $this->assets->register('bar', function($asset) {
+            $asset->style()
+                ->file(__DIR__ . '/fixtures/bar.css');
+            $asset->dependsOn('foo');
+        });
+
+        $this->resolver->resolve('bar');
+
+        $this->assertEquals(
+            file_get_contents(__DIR__ . '/fixtures/compiled-basic.css'),
+            $this->renderer->styles($this->resolver->assets())
+        );
+    }
+
+}
